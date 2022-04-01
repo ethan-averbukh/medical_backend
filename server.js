0cdb8c0dadd1784a9638f9d2f9db063d41c18a53
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-03-31 23:15:04 -0600

    Initialization

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+
+const PORT = process.env.PORT
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.use(cors());
+
+
+const journalController = require('./controllers/journalController');
+app.use('/Journal', journalController);
+
+const noteController = require('./controllers/noteController');
+app.use('/Notes', noteController)
+
+app.get('/', (req,res) => {
+    res.send('hello earth!')
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Listening in on port: ${PORT}`)
+})
